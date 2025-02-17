f70f88e566171246d6967cf96550ad278f9b224d
Pavan Santosh <nrpsantosh@gmail.com>
2025-02-17 03:53:21 +0000

    fact and dim tables

---
+++ stock-dbt/models/core/fact_stocks.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='stock_id',  
+        incremental_strategy='merge' 
+    )
+}}
+
+WITH historic_data AS (
+    SELECT * FROM {{ ref('stg_historical_data') }}
+),
+
+daily_data AS (
+    SELECT * FROM {{ ref('stg_daily_data') }}
+),
+
+combined_data AS (
+    SELECT * FROM historic_data
+    UNION ALL
+    SELECT * FROM daily_data
+)
+
+SELECT * FROM combined_data
+
+{% if is_incremental() %}
+WHERE date > (SELECT MAX(date) FROM {{ this }})
+{% endif %}
